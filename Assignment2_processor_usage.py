5ea01e513d98a690813724837bdcd9069be1ede4
aneeshb1989 <aneeshb1989@gmail.com>
2025-11-28 09:41:38 +0530

    removed unused variables

--- Assignment2_processor_usage.py
+++ Assignment2_processor_usage.py
@@ -1,13 +1,14 @@
 # Assignment2 - Processor usage program
 import psutil
 
-a = 0
 x = psutil.cpu_count()
 print(f"No of cores of the cpus is {x}")
+monitor = True
 
-while True:
+while monitor==True:
  y = psutil.cpu_percent(interval=3, percpu=None)
  print(f"Cpu usage is {y:.2f}%")
+
  if y > 15:
   print(f"CPU usage is {y:.2f}% and it exceeds threshold of 15%")
   
