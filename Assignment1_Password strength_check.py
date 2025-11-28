2b8f4f8a076f24072892ad84d334c34aede387c6
aneeshb1989 <aneeshb1989@gmail.com>
2025-11-28 09:21:19 +0530

    added comment heading on assignment1

--- Assignment1_Password strength_check.py
+++ Assignment1_Password strength_check.py
@@ -1,7 +1,7 @@
+# Assignment 1 - Password strength program
 def password_strength(password):
-    
     pass_val = False
-    while pass_val == False:
+    while pass_val == False and password != 'q':
     
       for y in str(password):
          contains_num = y.isdigit()
@@ -30,20 +30,24 @@ def password_strength(password):
                         pass_val = True
                      else:
                          print("Invalid password, first charecter shoud be upper case alphabet")
-                         password = input("Enter a valid password: ")   
+                         password = input("Enter a valid password, press q to quit: ")   
                   else:
                      print("Invalid password, password should contain atleast one special charecter")
-                     password = input("Enter a valid password: ")                 
+                     password = input("Enter a valid password, press q to quit: ")                 
                else:
                   print("Invalid password, password doesnt contain alphabets")
-                  password = input("Enter a valid password: ") 
+                  password = input("Enter a valid password, press q to quit: ") 
          else:
             print(f"Invalid password, password length is just {length}")
-            password = input("Enter a valid password: ") 
+            password = input("Enter a valid password, press q to quit: ") 
       else:
          print("Invalid password, doesnt contain numerals")
-         password = input("Enter a valid password: ") 
+         password = input("Enter a valid password, press q to quit: ") 
 
-
-password = input("Enter the password: ")
+print("""Please setup a strong password:
+         Minimum length: The password should be at least 8 characters long.
+         Contains both uppercase and lowercase letters.
+         Contains at least one digit (0-9).
+         Contains at least one special character (e.g., !, @, #, $, %)""")
+password = input("Enter the password, press q to quit: ")
 password_strength(password)
